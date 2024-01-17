2bb55586469feec16027730ce5dc941d35e01a64
KalybekovIlim <114552603+KalybekovIlim@users.noreply.github.com>
2024-01-17 14:39:18 +0600

    4commit open.py

--- practics/open.py
+++ practics/open.py
@@ -52,4 +52,13 @@ class Solution:
         elif list2:
             tail.next = list2
         return dumy.next
-            
+
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        l = 1
+
+        for r in range(1, len(nums)):
+            if nums[r] != nums[r - 1]:
+                nums[l] = nums[r]
+                l += 1
+        return 1
